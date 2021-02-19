8ae29a961238140cd7b19c7f021257c2b2e86d7e
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-19 10:38:28 -0500

    exercise 1-10 from the K&R

---
+++ exercise 1-10.c
@@ -0,0 +1,52 @@
+// Exercise 1-10 of the K&R
+/* "Write a program to copy its input to its output, replacing each tab by \t , each
+backspace by \b , and each backslash by \\ . This makes tabs and backspaces visible in an
+unambiguous way." */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c;
+    
+    while((c = getchar()) != EOF)
+    {
+        if ((c == '\t'))
+        {
+            if (c == '\t')
+            {
+                putchar('\\');
+                putchar('t');
+            }
+        }
+        // this option can't work since shell doesn't send backpace char to program as valid input.
+        else if (c == '\b')
+            {
+                putchar('\\');
+                putchar('b');
+            }
+        else if (c == '\\')
+        {
+            putchar('\\');
+            putchar('\\');
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
