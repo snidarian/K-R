7275fc30c75e4f68c6135e261f898a22715123d7
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-25 07:44:04 -0500

    Update exercise 1-13.c

--- exercise 1-13.c
+++ exercise 1-13.c
@@ -9,25 +9,31 @@
 
 int main()
 {
+    // State beggings in OUT -- making no assumptions about user my begin to type
     int state = OUT;
+    // Three iterant values making easier and more coherent iteration
     int c, j, k;
     int i = 0;
-
+    
+    // word length will be incremented while state == IN and reset when state set to OUT
     int wordlen = 0;
+    // input length is the array storing the values of the various word lengths in the sentence
     int inputlengths[40];
 
+    // note that the control flow is constantle setting and reseting state for each char.
+    // There is a way around this but I believe it's harmless and saved a great deal of likely wasted effort
     while ((c = getchar()) != EOF)
     {
         
         if (c == ' ' || c == '\t' || c == '\n')
         {
             state == OUT;
-            
+            // input value at subscript i set to current word length
             inputlengths[i] = wordlen;
             
             i++;
             //printf("i = %d\n", i);
-
+            // wordlength reset in anticipation of next word
             wordlen = 0;
         }
         else
@@ -38,7 +44,7 @@ int main()
         }
         
     }
-
+    // histogram print structure
     printf("|---------------------------------------\n");
     printf("|_______________________________________\n");
     printf("|word No.|     Chars  \n");
