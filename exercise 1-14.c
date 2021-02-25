f852c3ec1eeffa6fdc578a2e89f71e3bfb7a82eb
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-25 17:55:44 -0500

    Exercise 1-14 from the K&R
    Example of output from program:
    
    _____________________________________________________________
    
    ' occurred   2 times
    ; occurred  41 times
    a occurred  37 times
    d occurred  34 times
    f occurred 122 times
    g occurred  35 times
    h occurred  24 times
    j occurred  40 times
    k occurred  35 times
    l occurred  39 times
    o occurred   1 times
    p occurred   1 times
    r occurred   1 times
    s occurred  37 times
    w occurred   1 times
    
    =========================================================
    
    CHAR FREQUENCY HISTOGRAM:
    
    ' - ##
    
    ; - #########################################
    
    a - #####################################
    
    d - ##################################
    
    f - ##########################################################################################################################
    
    g - ###################################
    
    h - ########################
    
    j - ########################################
    
    k - ###################################
    
    l - #######################################
    
    o - #
    
    p - #
    
    r - #
    
    s - #####################################
    
    w - #
    
    ============================================================

---
+++ exercise 1-14.c
@@ -0,0 +1,70 @@
+// Exercise 1-14 from the K&R
+// "Write a program to print a histogram of the frequencies of different characters in its input"
+
+#include <stdio.h>
+
+
+
+int charfreq[128];
+
+
+int main()
+{
+    int c, i, j;
+    // initialize all values in the array to zero
+    for(i=0;i<128;i++)
+    {
+        charfreq[i] = 0;
+    }
+        
+    while((c = getchar())!= EOF)
+    {
+        charfreq[c]+= 1;
+    }
+
+    printf("______________________________________________________________\n\n");
+    // 32 is beginning of printable ascii characters. 128 = delete key but left it at 128.
+    for(i=32;i<128; i++)
+    {
+        
+        if (charfreq[i] > 0)
+        {
+        printf("%c occurred %3d times\n", i, charfreq[i]);
+        }
+    }
+
+    printf("\n");
+    printf("=========================================================\n\n");
+
+    printf("CHAR FREQUENCY HISTOGRAM:\n\n");
+    // This for loop will iterate all the way from 32 to 127 and print #'s based on char frequency
+    for(i=32;i<127; i++)
+    {
+        
+        if (charfreq[i] > 0)
+        {
+            printf("%c - ", i);
+
+            j = 0;
+            while(j < charfreq[i])
+            {
+                
+                printf("#");
+                
+                j++;
+            }
+                printf("\n\n");
+        }
+    }
+    printf("============================================================\n");
+
+    return 0;
+
+}
+
+
+
+
+
+
+
