82b70f1227c1bedeaa975b374cdb40f9a8d1475d
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-01 17:11:11 -0500

    Update exercise 1-16.c

--- exercise 1-16.c
+++ exercise 1-16.c
@@ -1,3 +1,7 @@
+// Exercise 1-16 from the K&R
+// Revise the main routine of the longest-line program so it will correctly print
+// the length of arbitrary long input lines, and as much as possible of the text.
+
 #include <stdio.h>
 #define MAXLINE 1000
 
